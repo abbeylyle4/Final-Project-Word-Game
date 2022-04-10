ebe23d75325d76b41e9ef241f362f98180c49916
GigiFitiany <gfitiany@gmail.com>
2022-04-10 13:46:44 -0400

    check 4

--- src/script.js
+++ src/script.js
@@ -23,7 +23,7 @@ function initLayout(){
         	let row = document.createElement("div")
         	row.className = "wordRow"
         
-	//5 boxxes for each guess 
+	//5 boxes for each guess 
         for (let j = 0; j < 5; j++) {
             let box = document.createElement("div")
             box.className = "letterBox"
@@ -33,5 +33,59 @@ function initLayout(){
         layout.appendChild(row)
     }
 }
+
+
+function addLetter (pressedKey) {
+    if (nextLetter === 5) { //Already filled
+        return
+    }
+    pressedKey = pressedKey.toLowerCase()
+
+    let row = document.getElementsByClassName("wordRow")[6 - guessesRemaining]
+    let box = row.children[newLetterIndex]
+    box.textContent = pressedKey
+    box.classList.add("usedBox")
+    currentGuess.push(pressedKey)
+    newLetterIndex += 1
+}
+
+function removeLetter () {
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let box = row.children[nextLetter - 1]
+    box.textContent = ""
+    box.classList.remove("usedBox")
+    currentGuess.pop()
+    newLetterIndex -= 1
+}
+
+
+
+
+document.addEventListener("keyup", (e) => {
+
+    if (guessesRemaining === 0) {
+        return
+    }
+
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspace" && newLetterIndex !== 0) {
+        removeLetter()
+        return
+    }
+
+    /* need to hook up with backend
+     if (pressedKey === "Enter") {
+        checkGuess()
+        return
+     }
+    */
+
+    let allowedInput= pressedKey.match(/[a-z]/gi)
+    if (!allowedInput|| allowedInput.length > 1) { //stops numbers and shift/cntrl from being inputted
+        return
+    } else {
+        addLetter(pressedKey)
+    }
+})
 
 initBoard();
