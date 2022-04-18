7c3245fdb02dac5c09ba804b6d240baf1012a4c5
abbeylyle4 <59837979+abbeylyle4@users.noreply.github.com>
2022-04-17 21:38:00 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -49,7 +49,7 @@ function addLetter (pressedKey) {
 }
  
 function removeLetter () {
-   let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+   let row = document.getElementsByClassName("wordRow")[6 - guessesRemaining]
    let box = row.children[nextLetter - 1]
    box.textContent = ""
    box.classList.remove("usedBox")
@@ -86,5 +86,94 @@ document.addEventListener("keyup", (e) => {
        addLetter(pressedKey)
    }
 })
+ 
+ 
+function shadeKeyBoard(letter, color) {
+   for (const elem of document.getElementsByClassName("keyButton")) {
+       if (elem.textContent === letter) {
+           let oldColor = elem.style.backgroundColor
+           if (oldColor === 'green') {
+               return
+           }
+ 
+           if (oldColor === 'yellow' && color !== 'green') {
+               return
+           }
+ 
+           elem.style.backgroundColor = color
+           break
+       }
+   }
+}
+ 
+ 
+ 
+ 
+//Checking the guess ******** Long code chunk
+ 
+function checkGuess () {
+   let row = document.getElementsByClassName("wordRow")[6 - guessesRemaining]
+   let guessWord = ''
+   let Answer= Array.from(AnswerWord)
+ 
+   for (const val of currentGuess) {
+       guessWord += val
+   }
+ 
+   if (guessString.length != 5)
+       return;
+  
+ 
+   if (!WORDS.includes(guessWord)) {
+       alert("Is not a word")
+       return
+   }
+ 
+  
+   for (let i = 0; i < 5; i++) {
+       let letterColor = ''
+       let box = row.children[i]
+       let letter = currentGuess[i]
+      
+       let letterPlace = Answer.indexOf(currentGuess[i])
+       // is letter in the correct guess
+       if (letterPlace === -1) { //Not even in the word
+           letterColor = 'grey'
+       } else {
+           // letter in word
+           // if letter index and right guess index are the same
+           // letter is in the right position
+           if (currentGuess[i] === rightGuess[i]) {
+               letterColor = 'green'
+           } else {
+               // Right letter, wrong place
+               letterColor = 'yellow'
+           }
+ 
+           rightGuess[letterPlace] = "!"
+       }
+ 
+       let delay = 150 * i
+       setTimeout(()=> {
+           //shade box
+           box.style.backgroundColor = letterColor
+           shadeKeyBoard(letter, letterColor)
+       }, delay)
+   }
+ 
+   if (guessWord === Answer) {
+       alert("That is correct")
+       guessesRemaining = 0
+       return
+   } else {
+       guessesRemaining -= 1;
+       currentGuess = [];
+       nextLetter = 0;
+ 
+       if (guessesRemaining === 0) {
+           alert(`The word was: "${Answer}"`)
+       }
+   }
+}
  
 initBoard();
