49a5ef2c3e56fcf6f434643caa77e598efaa4b36
abbeylyle4 <59837979+abbeylyle4@users.noreply.github.com>
2022-04-28 12:53:01 -0400

    counts

--- api/addData.js
+++ api/addData.js
@@ -17,11 +17,13 @@ export default async function handler(req, res) {
   // take the id that comes back and then apply to the user object
   woordle2.id = dbResult.insertId
   const [metrics] = await conn.query('select * from woordle2');
+  const [wincount] = await conn.query('select * from woordle2 where result = 1');
+  const [triescount] = await conn.query('select AVG(tries) from woordle2');
   const data = {
-      players: 50,
-      wins: 20,
-      tries: 250
+      players: metrics.length,
+      wins: wincount.length,
+      tries: triescount[0]
   }
-  res.json(woordle2);
+  res.json(data);
 
 }
