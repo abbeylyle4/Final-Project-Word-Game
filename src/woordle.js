fcf1fb94e4644c88fe8556a9a41b7814c34833cd
abbeylyle4 <59837979+abbeylyle4@users.noreply.github.com>
2022-05-02 16:23:42 -0400

    delete key

--- src/woordle.js
+++ src/woordle.js
@@ -96,13 +96,13 @@
 
     if (currentWord === this.word) {
       if(this.guessedWords.length === 1)
-              window.alert("Congratulations, you guessed the right word in " + this.guessedWords.length + "guess! Game stats: " + this.result + " people have won. Average tries have been:" + this.tries );
+              window.alert("Congratulations, you guessed the right word in " + this.guessedWords.length + "guess! Game stats: " + this.result + " people have won. Average tries have been: " + this.tries );
               
       else 
       
     
            await this.updateOutcome(this.guessedWords.length,true)
-           window.alert("Congratulations, you guessed the right word in " + this.guessedWords.length + " guesses! Game stats: " + this.result + " people have won. Average tries have been:" + this.tries);
+           window.alert("Congratulations, you guessed the right word in " + this.guessedWords.length + " guesses! Game stats: " + this.result + " people have won. Average tries have been: " + this.tries);
     }
     else{
           window.alert("Wrong!");
@@ -111,7 +111,7 @@
     }
        
     if (this.guessedWords.length === 7){
-        window.alert ("sorry, you have no more guesses! The word is "+ this.word + "Game stats: " + this.result + " people have won.  Average tries have been:" + this.tries);
+        window.alert ("Sorry, you have no more guesses! The word is "+ this.word + "Game stats: " + this.result + " people have won.  Average tries have been: " + this.tries);
         this.updateOutcome(this.guessedWords.length,false)
     }
 
@@ -119,6 +119,13 @@
 } 
 
  handleDeleteLetter(){
+  const blockDelete = this.blockDelete;
+  if (!this.blockDelete){
+    this.blockDelete = true;
+  }
+  else
+  this.blockDelete = false;
+  
     const currentWordArr = this.getCurrentWordArr()
     const removedLetter = currentWordArr.pop()
 
